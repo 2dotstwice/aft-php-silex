cc7e71cedf6e8c80e0e120ade15e2e976555fc63
Bert Ramakers <bertramakers@me.com>
2015-10-11 17:40:32 +0200

    Added example routes from the slides.

--- web/index.php
+++ web/index.php
@@ -1,11 +1,55 @@
 <?php
 
+use \Symfony\Component\HttpFoundation\Request;
+use \Symfony\Component\HttpFoundation\Response;
+
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $app = new Silex\Application();
 
-$app->get('/hello', function () {
-    return 'Hello world!';
-});
+$app->get(
+    '/hello',
+    function (Request $request) {
+        return new Response('Hello world!');
+    }
+);
+
+$app->get(
+    '/hello/{name}',
+    function (Request $request, $name) {
+        return new Response('Hello ' . $name . '!');
+    }
+);
+
+$app->get(
+    '/search',
+    function (Request $request) {
+        $filter = $request->query->get('name');
+        $names = ['Bill Gates', 'Steve Jobs', 'Steve Wozniak'];
+
+        $matches = [];
+        foreach ($names as $name) {
+            if (stripos($name, $filter) !== false) {
+                $matches[] = $name;
+            }
+        }
+
+        return new Response(
+            count($matches) . ' result(s): ' . implode(', ', $matches)
+        );
+    }
+);
+
+$app->post(
+    '/contact',
+    function (Request $request) {
+        $filename = __DIR__ . '/files/contact-' . time() . '.txt';
+        $content = $request->getContent();
+
+        file_put_contents($filename, $content);
+
+        return new Response($content);
+    }
+);
 
 $app->run();
